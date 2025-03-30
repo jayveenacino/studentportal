b724709574c614c88f9f5669fa06d6439e233f73
John Thomas <johnthomasalog@gmail.com>
2025-03-30 20:04:42 +0800

    added studentNumber

--- server/index.js
+++ server/index.js
@@ -14,7 +14,11 @@ mongoose.connect("mongodb://127.0.0.1:27017/student", {
 
 app.post('/register', async (req, res) => {
     try {
-        const student = await StudentModel.create(req.body);
+        let studentNumber = await StudentModel.countDocuments() + 1;
+        studentNumber = studentNumber.toString().padStart(6, '0');
+
+
+        const student = await StudentModel.create({ ...req.body, studentNumber });
         res.status(201).json({ message: "Account created successfully", student });
     } catch (err) {
         res.status(500).json({ message: "Error creating account", error: err.message });
--- server/models/Student.js
+++ server/models/Student.js
@@ -11,6 +11,7 @@ const StudentSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     conpassword: String,
+    studentNumber: String
 });
 
 const StudentModel = mongoose.model("students", StudentSchema);
