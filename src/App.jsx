5618b6893f58ba7c855fcfb318d2d72ca0679497
raymayo <ormillada.jr@gmail.com>
2025-04-10 07:00:34 +0800

    fixed App.jsx routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,74 @@
-import { useState } from 'react'
-import Body from './Body'
-import Home from './Home'
-import Signup from './student/Signup'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Navigate } from 'react-router-dom'
-import Root from './Root'
-import Create from './student/Create'
-import Login from './student/Login'
-import Adminlogin from './Admin/Adminlogin'
-import Notice from './student/Notice'
-import AdminDashboard from './Admin/Admindashboard'
-import Preregister from './student/Preregister'
+import Body from './Body';
+import Home from './Home';
+import Signup from './student/Signup';
+import Create from './student/Create';
+import Login from './student/Login';
+import Adminlogin from './Admin/Adminlogin';
+import Notice from './student/Notice';
+import AdminDashboard from './Admin/Admindashboard';
+import Preregister from './student/Preregister';
 
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from 'react-router-dom';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route element={<Root />}>
-      {/* INDEX SESION */}
-      <Route index element={<Body />} />
-      <Route path="home" element={<Home />} />
-      {/* STUDENT SESION */}
-      <Route path="signup" element={<Signup />} />
-      <Route path="signup/create" element={<Create />} />
-      <Route path="login" element={<Login />} />
-      <Route path="preregister" element={<Preregister />} />
-      <Route path="login/notice" element={<Notice />} />
-      <Route path="preregister/notice" element={<Notice />} />
-      {/* ADMIN SESION */}
-      <Route path="adminlogin" element={<Adminlogin />} />
-      <Route path="admindashboard" element={<AdminDashboard />} />
-      <Route path="*" element={<Navigate to="/" />} />
-    </Route>
-  )
-)
+import Root from './Root';
 
-export default function App() {
+function App() {
+	return (
+		<Router>
+			<Routes>
+				{/* Root layout with nested routes */}
+				<Route path="/" element={<Root />}>
+					{/* INDEX SESSION */}
+					<Route index element={<Body />} />
+					<Route path="home" element={<Home />} />
 
-  return (
-    <RouterProvider router={router} />
-  )
+					{/* STUDENT SESSION */}
+					<Route path="signup" element={<Signup />} />
+					<Route path="signup/create" element={<Create />} />
+					<Route path="login" element={<Login />} />
+					<Route path="preregister" element={<Preregister />} />
+					<Route path="login/notice" element={<Notice />} />
+					<Route path="preregister/notice" element={<Notice />} />
+
+					{/* ADMIN SESSION */}
+					<Route path="adminlogin" element={<Adminlogin />} />
+					<Route path="admindashboard" element={<AdminDashboard />} />
+
+					{/* Fallback redirect */}
+					<Route path="*" element={<Navigate to="/" replace />} />
+				</Route>
+			</Routes>
+		</Router>
+	);
 }
+// const router = createBrowserRouter(
+// 	createRoutesFromElements(
+// 		<Route element={<Root />}>
+// 			{/* INDEX SESION */}
+// 			<Route index element={<Body />} />
+// 			<Route path="home" element={<Home />} />
+// 			{/* STUDENT SESION */}
+// 			<Route path="signup" element={<Signup />} />
+// 			<Route path="signup/create" element={<Create />} />
+// 			<Route path="login" element={<Login />} />
+// 			<Route path="preregister" element={<Preregister />} />
+// 			<Route path="login/notice" element={<Notice />} />
+// 			<Route path="preregister/notice" element={<Notice />} />
+// 			{/* ADMIN SESION */}
+// 			<Route path="adminlogin" element={<Adminlogin />} />
+// 			<Route path="admindashboard" element={<AdminDashboard />} />
+// 			<Route path="*" element={<Navigate to="/" />} />
+// 		</Route>
+// 	)
+// );
+
+// export default function App() {
+// 	return <RouterProvider router={router} />;
+// }
+
+export default App;
--- src/Root.jsx
+++ src/Root.jsx
@@ -1,23 +1,34 @@
-import React from 'react'
-import Nav from './Nav'
-import Footer from './Footer'
-import { Outlet, useLocation } from 'react-router-dom'
+import React from 'react';
+import Nav from './Nav';
+import Footer from './Footer';
+import { Outlet, useLocation } from 'react-router-dom';
 
 export default function Root() {
-    const location = useLocation()
+	const location = useLocation();
 
-    return (
-        <>
+	//COMMENT: Bad practice to use pathname to determine the component to render
+	//COMMENT: Use a better way to determine the component to render
 
-            {(location.pathname !== "/signup" && location.pathname !== "/signup/create" && location.pathname !== "/login" && location.pathname !== "/adminlogin" && location.pathname !== "/login/notice" && location.pathname !== "/admindashboard" && location.pathname !== "/preregister" && location.pathname !== "/preregister/notice") &&
-                <Nav />
-            }
+	return (
+		<>
+			{location.pathname !== '/signup' &&
+				location.pathname !== '/signup/create' &&
+				location.pathname !== '/login' &&
+				location.pathname !== '/adminlogin' &&
+				location.pathname !== '/login/notice' &&
+				location.pathname !== '/admindashboard' &&
+				location.pathname !== '/preregister' &&
+				location.pathname !== '/preregister/notice' && <Nav />}
 
-            <Outlet />
-            {(location.pathname !== "/signup" && location.pathname !== "/signup/create" && location.pathname !== "/login" && location.pathname !== "/adminlogin" && location.pathname !== "/login/notice" && location.pathname !== "/admindashboard" && location.pathname !== "/preregister" && location.pathname !== "/preregister/notice") &&
-                <Footer />
-            }
-
-        </>
-    )
+			<Outlet />
+			{location.pathname !== '/signup' &&
+				location.pathname !== '/signup/create' &&
+				location.pathname !== '/login' &&
+				location.pathname !== '/adminlogin' &&
+				location.pathname !== '/login/notice' &&
+				location.pathname !== '/admindashboard' &&
+				location.pathname !== '/preregister' &&
+				location.pathname !== '/preregister/notice' && <Footer />}
+		</>
+	);
 }
