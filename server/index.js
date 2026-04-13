b77e4570e8c44c452518aec2501a95df902bf4ca
Jayvee Madriaga Nacino <jayveemadriaganacino@gmail.com>
2026-04-13 19:57:18 +0800

    cors

--- server/index.js
+++ server/index.js
@@ -26,13 +26,19 @@ const morgan = require("morgan");
 
 require('dotenv').config({ quiet: true });
 
-const app = express();
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    origin: 'https://kolehiyongsubic.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
+
+const allowedOrigins = [
+    'https://kolehiyongsubic.vercel.app',
+    'http://localhost:5173',  // for local development
+    'http://localhost:3000'
+];
+;
 app.options('*', cors());
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
