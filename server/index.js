cbbe4f73dd15d8d29a4dbaed50b6c1303445a4dd
Jayvee Madriaga Nacino <jayveemadriaganacino@gmail.com>
2026-04-14 18:26:19 +0800

    back code

--- server/index.js
+++ server/index.js
@@ -27,12 +27,7 @@ const morgan = require("morgan");
 require('dotenv').config({ quiet: true });
 
 const app = express();
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
-app.options('*', cors())
+app.use(cors());
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(morgan('tiny'))
